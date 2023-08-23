8d1c0943656718c10b981def873f2918bd89a708
Izanatkg <uliseseter@gmail.com>
2023-08-23 15:49:17 -0700

    first commit

---
+++ gif-app/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Gifapp } from './Gifapp'
+import './Styles.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Gifapp />
+  </React.StrictMode>,
+)
